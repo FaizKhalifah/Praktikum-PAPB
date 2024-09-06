416086608a13eecb5d2712961745024efd610e7b
Faiz Khalifah <faiz_doc@outlook.com>
2024-09-06 21:44:05 +0700

    testing commit praktikum2

--- app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
+++ app/src/main/java/com/tifd/projectcomposed/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tifd.projectcomposed.ui.theme.ProjectComposeDTheme
@@ -43,9 +45,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyScreen() {
-    var text by remember { mutableStateOf("") }
-    var inputText by remember { mutableStateOf("") }
-
+    var nama by remember { mutableStateOf("") }
+    var jurusan by remember { mutableStateOf("") }
+    var sapaan by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -53,19 +55,38 @@ fun MyScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = text)
-        Spacer(modifier = Modifier.height(16.dp))
-        TextField(
-            value = inputText,
-            onValueChange = { inputText = it },
-            label = { Text("Enter text") }
+        Text(
+            text="Aplikasi penyapa",
+            color=Color.Black
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            text = inputText
-        }) {
+        TextField(
+            value = nama,
+            onValueChange = { nama = it },
+            label = { Text("Masukkan namamu") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = jurusan,
+            onValueChange = { jurusan = it },
+            label = { Text("Masukkan jurusanmu") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = sapaan,
+            color= Color.Gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = { sapaan = "Halo $nama dari jurusan $jurusan" },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Cyan,
+                contentColor = Color.DarkGray
+            )
+        ) {
             Text("Submit")
         }
+
     }
 }
 
